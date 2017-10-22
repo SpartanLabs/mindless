23f76183c20bf4983abe813ac40782dcbf3309d3
Zachary Rosenthal <rosent76@msu.edu>
2017-10-22 13:17:13 -0400

    remaing artifacts from project this was ripped out of

--- lib/providers.ts
+++ lib/providers.ts
@@ -1,9 +1,6 @@
-import { UserTable } from '../app/models/user';
 import { Container } from 'inversify';
-
 import { Dynamo } from './data';
 
 export const registerProviders = (container: Container) => {
     container.bind<Dynamo>(Dynamo).to(Dynamo);
-    container.bind<UserTable>(UserTable).to(UserTable);
 }
