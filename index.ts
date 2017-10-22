354588c5830566fb42c88aca10d3077456777d3b
Zachary Rosenthal <rosent76@msu.edu>
2017-10-22 13:10:15 -0400

    more barrel exports

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 export { Dynamo, DynamoTable, ModelFactory } from './lib/data';
 export { Router, Routes } from './lib/routing';
 export { Context } from './lib/interfaces';
-export { Request } from './lib/request';
+export { Request, Event } from './lib/request';
 export { Response } from './lib/response';
 export { Middleware } from './lib/middleware/middleware';
 export { Controller } from './lib/controller/controller';
