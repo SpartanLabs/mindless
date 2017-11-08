36e3a8bcd7b45d08b1e3d72b0a969a9bddf3d907
Zachary Rosenthal <rosent76@msu.edu>
2017-11-08 18:46:50 -0500

    some quick fixes (#5)

--- lib/middleware/middleware.ts
+++ lib/middleware/middleware.ts
@@ -1,8 +1,11 @@
+import { injectable } from 'inversify';
 import { Request } from '../request';
+
+@injectable()
 export abstract class Middleware {
   constructor() { }
 
-  protected independent : boolean = true;
+  protected independent: boolean = true;
 
   public isIndependent() {
     return this.independent;
