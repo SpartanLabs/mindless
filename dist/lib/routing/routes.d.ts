07d757140eaacf0b3368cf77560f903b6aec484a
Zachary Rosenthal <rosent76@msu.edu>
2017-11-08 18:50:51 -0500

    opps forgot to buld (#6)

--- dist/lib/routing/routes.d.ts
+++ dist/lib/routing/routes.d.ts
@@ -1,21 +1,22 @@
 import { GenericConstructor } from '../interfaces';
 import { Middleware } from '../middleware/middleware';
 import { Controller } from '../controller/controller';
-export declare type Route<M, C> = {
+export interface Route<M extends Middleware, C extends Controller> {
     controller: GenericConstructor<C>;
     function: string;
     middleware?: GenericConstructor<M>[];
-};
-export declare type MindlessRoutes = Routes<Middleware, Controller>;
-export declare type Routes<M, C> = {
+}
+export interface Routes<M extends Middleware, C extends Controller, R extends Route<M, C>> {
     middleware?: GenericConstructor<M>[];
     routes: {
         [key: string]: {
             middleware?: GenericConstructor<M>[];
-            post?: Route<M, C>;
-            get?: Route<M, C>;
-            put?: Route<M, C>;
-            delete?: Route<M, C>;
+            post?: R;
+            get?: R;
+            put?: R;
+            delete?: R;
         };
     };
-};
+}
+export declare type MindlessRoute = Route<Middleware, Controller>;
+export declare type MindlessRoutes = Routes<Middleware, Controller, MindlessRoute>;
