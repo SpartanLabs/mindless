8745991e7b49434a0bac1204b1a5867396885ca5
Evan Moran <moraneva@msu.edu>
2017-12-17 16:47:05 -0500

    added tests for export members

---
+++ tests/export.tests.ts
@@ -0,0 +1,4 @@
+import { registerProviders, Dynamo, DynamoTable, ModelFactory, Model, Route, Router,
+     MindlessRoute, RouteUrl, Context, Request, Event, HttpMethods, Response, 
+     Middleware, Controller, MINDLESS_SERVICE_INDENTIFIERS, MindlessConfig, 
+     DefaultConfig } from '../'
