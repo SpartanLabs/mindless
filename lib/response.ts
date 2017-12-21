dca79291755346421dccaaad0f10ef26629baf07
Evan Moran <moraneva@msu.edu>
2017-12-21 09:21:53 -0500

    Added method onto response to transform into AWS lambda response (#35)

--- lib/response.ts
+++ lib/response.ts
@@ -1,14 +1,28 @@
+export interface AWSLambdaIntegrationResponse {
+  statusCode: number;
+  body: string;
+  headers: { [key: string]: string };
+}
+
 export class Response {
 
-    /**
-     * 
-     * @param statusCode 
-     * @param headers 
-     * @param body 
-     */
-    public constructor(
-      public statusCode: number = 200,
-      public body: {[key: string]: any}  = {},
-      public headers: {[key: string]: string} = {}
-    ) { }
+  /**
+   * 
+   * @param statusCode 
+   * @param headers 
+   * @param body 
+   */
+  public constructor(
+    public statusCode: number = 200,
+    public body: { [key: string]: any } = {},
+    public headers: { [key: string]: string } = {}
+  ) { }
+
+  public toAWSLambdaIntegrationResponse(): AWSLambdaIntegrationResponse {
+    return {
+      statusCode: this.statusCode,
+      body: JSON.stringify(this.body),
+      headers: this.headers
+    }
   }
+}
