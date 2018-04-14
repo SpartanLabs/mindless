1abd7f2c59b9362859cc430605b33ec2f94eb343
Zachary Rosenthal <rosent76@msu.edu>
2018-04-14 19:28:02 -0400

    made model shit infinitley better, played around with decorators

--- lib/data/dynamo/table.ts
+++ lib/data/dynamo/table.ts
@@ -1,133 +1,136 @@
-import { DynogelsItemCallback, CreateItemOptions, Model, ModelConfiguration, Document, DocumentCollection, UpdateItemOptions, DestroyItemOptions, GetItemOptions } from 'dynogels';
+import { DynogelsItemCallback, CreateItemOptions, Model as DynModel, ModelConfiguration, Document, DocumentCollection, UpdateItemOptions, DestroyItemOptions, GetItemOptions } from 'dynogels';
 import { injectable, inject } from 'inversify';
 
 import { Dynamo } from './dynamo';
 import { MINDLESS_SERVICE_INDENTIFIERS } from '../../types';
+import {Model, ModelConstructor} from "../model";
+
 
 @injectable()
-export abstract class DynamoTable<T> {
+export abstract class DynamoTable<TModel extends Model> {
     protected abstract tableName: string;
     protected abstract definition: ModelConfiguration;
-    public model: Model;
+    public dynModel: DynModel;
 
-    constructor( @inject(MINDLESS_SERVICE_INDENTIFIERS.Dynamo) private dynamo: Dynamo) {
-    }
+    constructor(
+        @inject(MINDLESS_SERVICE_INDENTIFIERS.Dynamo) private dynamo: Dynamo,
+        protected TConstructor: ModelConstructor<TModel>
+    ) { }
 
     protected registerTable() {
-        this.model = this.dynamo.addDefinition(this.tableName, this.definition);
+        this.dynModel = this.dynamo.addDefinition(this.tableName, this.definition);
         // this.dynamo.createTables();
     }
 
-    public create(data: { [key: string]: {} }, options: CreateItemOptions = {}): Promise<T> {
+    public create(data: Model, options: CreateItemOptions = {}): Promise<TModel> {
         let promiseCallback = (resolve, reject) => {
-            let createModelCallback: DynogelsItemCallback = (err, model) => {
+            let createModelCallback: DynogelsItemCallback = (err, document) => {
                 if (err) {
-                    console.error('Error creating a entry on ' + this.tableName + ' table. Err: ', err);
+                    console.error(`Failed to create item on ${this.tableName} table.\n Item: ${JSON.stringify(data.model)}\n Err: `, err);
                     reject(err);
                 } else {
-                    let m: T = this.transformToModel(model);
-                    resolve(m);
+                    resolve(data);
                 }
-            }
+            };
 
-            this.model.create(data, options, createModelCallback);
+            this.dynModel.create(data.model, options, createModelCallback);
         };
 
         return new Promise(promiseCallback)
     }
 
-    public getAll(): Promise<T[]> {
-        let transform = (models: Document[]) => models.map(model => this.transformToModel(model));
-        return this.getAllBase(transform);
+    public getAll(): Promise<TModel[]> {
+        const documentMapper = (documents: Document[]) =>
+            documents.map(document => new this.TConstructor(document));
+        return this.getAllBase(documentMapper);
     }
 
     public getAllRaw(): Promise<{}[]> {
         return this.getAllBase(x => x);
     }
 
-    public getItems(items: any[], options: GetItemOptions = {}): Promise<T[]> {
-        let transform = (models: T[]) => models.map(model => this.transformToModel(model));
+    public getItems(items: any[], options: GetItemOptions = {}): Promise<TModel[]> {
 
         let promiseCallback = (resolve, reject) => {
-            let callback = (err, items: any[]) => {
+            let callback = (err, documents: any[]) => {
                 if (err) {
                     console.error(`Error getting items on ${this.tableName} table. Err: ${err}`);
                     reject(err);
                 }
                 else {
-                    items = transform(items);
-                    resolve(items);
-                }
-            }
-
-            this.model.getItems(items, options, callback);
-        }
-
-        return new Promise(promiseCallback);
-    }
-
-    public get(hashKey: string, options: GetItemOptions = {}, rangeKey?: string): Promise<T> {
-
-        let promiseCallback = (resolve, reject) => {
-            let callback = (err, item) => {
-                if (err) {
-                    console.error(`Error getting items on ${this.tableName} table. Err: ${err}`);
-                    reject(err);
-                }
-                else {
-                    item = this.transformToModel(item);
-                    resolve(item);
-                }
-            }
-
-            if (rangeKey == null) {
-                this.model.get(hashKey, options, callback);
-            }
-            else {
-                this.model.get(hashKey, rangeKey, options, callback);
-            }
-        }
-
-        return new Promise(promiseCallback);
-    }
-
-    protected getAllBase(transform: (x: any[]) => any): Promise<any[]> {
-        let promiseCallback = (resolve, reject) => {
-
-            let callback = (err, models: DocumentCollection) => {
-                if (err) {
-                    console.error('Error retrieving all models on ' + this.tableName + ' table. Err: ', err);
-                    reject(err);
-                } else {
-                    models = transform(models.Items);
+                    const models = documents.map(document => new this.TConstructor(document));
                     resolve(models);
                 }
             };
 
-            this.model.scan().loadAll().exec(callback);
+            this.dynModel.getItems(items, options, callback);
         };
 
         return new Promise(promiseCallback);
     }
 
-    public update(data: { [key: string]: {} }, options: UpdateItemOptions = {}): Promise<T> {
+    public get(hashKey: string, options: GetItemOptions = {}, rangeKey?: string): Promise<TModel> {
+
+        let promiseCallback = (resolve, reject) => {
+            let callback = (err, document) => {
+                if (err) {
+                    console.error(`Error getting items on ${this.tableName} table. Err: ${err}`);
+                    reject(err);
+                }
+                else {
+                    const model = new this.TConstructor(document);
+                    resolve(model);
+                }
+            };
+
+            if (rangeKey == null) {
+                this.dynModel.get(hashKey, options, callback);
+            }
+            else {
+                this.dynModel.get(hashKey, rangeKey, options, callback);
+            }
+        };
+
+        return new Promise(promiseCallback);
+    }
+
+    protected getAllBase(documentMapper: (x: any[]) => any): Promise<any[]> {
         let promiseCallback = (resolve, reject) => {
 
-            let callback: DynogelsItemCallback = (err, item) => {
+            let callback = (err, documentCollection: DocumentCollection) => {
+                if (err) {
+                    console.error('Error retrieving all models on ' + this.tableName + ' table. Err: ', err);
+                    reject(err);
+                } else {
+                    const models = documentMapper(documentCollection.Items);
+                    resolve(models);
+                }
+            };
+
+            this.dynModel.scan().loadAll().exec(callback);
+        };
+
+        return new Promise(promiseCallback);
+    }
+
+    public update(data: TModel, options: UpdateItemOptions = {}): Promise<TModel> {
+        let promiseCallback = (resolve, reject) => {
+
+            let callback: DynogelsItemCallback = (err, document) => {
                 if (err) {
                     console.error('Error updating item on ' + this.tableName + ' table. Err: ', err);
                     reject(err);
                 } else {
-                    resolve(this.transformToModel(item));
+                    resolve(data);
                 }
             };
 
-            this.model.update(data, options, callback);
-        }
+            this.dynModel.update(data.model, options, callback);
+        };
         return new Promise(promiseCallback);
     }
 
-    public delete(hashKey: string, rangeKey?: string, options: DestroyItemOptions = {}): Promise<T> {
+    public delete(hashKey: string, rangeKey?: string, options: DestroyItemOptions = {}): Promise<TModel> {
         let promiseCallback = (resolve, reject) => {
 
             let callback: DynogelsItemCallback = (err, item) => {
@@ -140,14 +143,12 @@ export abstract class DynamoTable<T> {
             };
 
             if (rangeKey == null) {
-                this.model.destroy(hashKey, options, callback);
+                this.dynModel.destroy(hashKey, options, callback);
             }
             else {
-                this.model.destroy(hashKey, rangeKey, options, callback);
+                this.dynModel.destroy(hashKey, rangeKey, options, callback);
             }
-        }
+        };
         return new Promise(promiseCallback);
     }
-
-    protected abstract transformToModel(createdModel: any): T;
 }
--- tests/providers.test.ts
+++ tests/providers.test.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 import { MINDLESS_SERVICE_INDENTIFIERS } from '../lib/types';
 import { DefaultConfig, MindlessConfig } from '../lib/configs';
 import { registerProviders } from '../lib/providers';
-import { Container, injectable } from "inversify";
+import {Container, injectable} from "inversify";
 
 describe('registerProviders', () => {
 
@@ -31,4 +31,68 @@ describe('registerProviders', () => {
 
         expect(container.get<MindlessConfig>(MINDLESS_SERVICE_INDENTIFIERS.MindlessConfig).dynamoEndpoint).toEqual("MyCustomEndpoint");
     });
+});
+
+describe('playground', () => {
+    const ModelPropertyMetaDataKey = Symbol("DataMember");
+
+    function ModelProperty() {
+        return (target, property) => {
+            const props = Reflect.getMetadata(ModelPropertyMetaDataKey, target) || [];
+            props.push(property);
+            Reflect.defineMetadata(ModelPropertyMetaDataKey, props, target);
+        }
+    }
+
+    function GetDataMembers(target: any) {
+        return Reflect.getMetadata(ModelPropertyMetaDataKey, target);
+    }
+
+    class A {
+        constructor(data: {[key: string]: any}) {
+            const props = GetDataMembers(this);
+            console.log("props: ", props);
+            Object.keys(data)
+                .filter(prop => props.some(p => p === prop))
+                .forEach(prop => this[prop] = data[prop]);
+        }
+
+        get model() {
+            const props =  Object.keys(this);
+            return props.reduce((obj, prop) => {
+                obj[prop] = this[prop];
+                return obj;
+            }, {});
+        }
+    }
+
+    class A2 {
+        constructor(data: {[key: string]: any}) {
+            Object.keys(data)
+                .forEach(prop => this[prop] = data[prop]);
+        }
+
+        get model() {
+            const props =  Object.keys(this);
+            return props.reduce((obj, prop) => {
+                obj[prop] = this[prop];
+                return obj;
+            }, {});
+        }
+    }
+
+    class B extends A2 {
+        //@ModelProperty()
+        prop1: string;
+
+        //@ModelProperty()
+        prop2: string;
+
+    }
+
+    const m = [['a', 1],['b', 2]];
+
+    const b = new B({prop1: 'hi', prop2: m, prop3: "hi"});
+    console.log('b model: ', b.model);
+
 });
