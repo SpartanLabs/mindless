c883419a68c3a0a33c094b32247ed414e732865a
Zachary Rosenthal <rosent76@msu.edu>
2018-05-12 16:21:13 -0400

    almost there i think

--- package.sh
+++ package.sh
@@ -1,11 +1,2 @@
 yarn build
-rm -rf package
-mkdir package
-mv dist package/
-cp package.json package/
-cp README.md package/
-cp yarn.lock package/
-cp LICENSE package/
-cd package
 npm publish
-cd ..
