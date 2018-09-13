17ff7a9feb703e5450478b043e4ac0aab8f39bd8
Martin <mshameti@emich.edu>
2018-09-13 10:25:24 -0400

    see if that works v2

--- src/request/request-interface.ts
+++ src/request/request-interface.ts
@@ -1,5 +1,5 @@
 export interface IRequest {
   get(key: string): any // retrieve request input
-  header(key: string): string // retrieve request header
+  header(key: string): string | undefined // retrieve request header
   add(key: string, value: any): void // add request data
 }
