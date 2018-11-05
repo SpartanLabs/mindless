b4e6d714b2366c0d0aa20ce718aa3b30d64efea1
zRosenthal <rosent76@msu.edu>
2018-11-05 11:25:54 -0500

    fix(mindless error and error handler): export things

--- src/mindless.ts
+++ src/mindless.ts
@@ -1,6 +1,8 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 
+export { MindlessError } from './error/mindless.error'
+export { CustomErrorHandler } from './error/custom-error-handler'
 export { App, IContainer, IApp } from './app'
 export { Router, Route, MindlessRoute, RouteUrl, IRouteUrl, IRouter } from './routing'
 export { Context } from './interfaces'
